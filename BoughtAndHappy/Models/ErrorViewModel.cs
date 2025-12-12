444d072a817c63bee95c604b3015fbdefcfd4884
matvisiv2 <matvisiv2@gmail.com>
2025-12-12 21:39:08 +0200

    Add project files.

---
+++ BoughtAndHappy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BoughtAndHappy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
