a233e62ee2a12381fbc4ac297c6376e04b537f5c
matvisiv2 <matvisiv2@gmail.com>
2025-12-14 14:01:29 +0200

    Add-Migration SeedProducts

--- BoughtAndHappy/Migrations/20251214115304_SeedProducts.Designer.cs
+++ BoughtAndHappy/Migrations/20251214115304_SeedProducts.Designer.cs
@@ -2,6 +2,7 @@
 using BoughtAndHappy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BoughtAndHappy.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251214115304_SeedProducts")]
+    partial class SeedProducts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
